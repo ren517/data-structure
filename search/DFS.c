fcc109df9c9cc3a30bcceeade3987713ba76575e
ren517 <2993159615@qq.com>
2024-12-24 15:36:02 +0800

    提交

---
+++ search/DFS.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+typedef int EdgeType;
+typedef char VertexType; // 顶点
+
+#define MAXSIZE 100
+int visited[MAXSIZE];
+
+typedef struct
+{
+    VertexType vertex[MAXSIZE];
+    EdgeType arc[MAXSIZE][MAXSIZE];
+    int vertex_num;
+    int edge_num;
+} Mat_Grph;
+
+void create_graph(Mat_Grph *G)
+{
+    G->vertex_num = 9;
+    G->edge_num = 15;
+    G->vertex[0] = 'A';
+    G->vertex[1] = 'B';
+    G->vertex[2] = 'C';
+    G->vertex[3] = 'D';
+    G->vertex[4] = 'E';
+    G->vertex[5] = 'F';
+    G->vertex[6] = 'G';
+    G->vertex[7] = 'H';
+    G->vertex[8] = 'I';
+
+    for (int i = 0; i < G->vertex_num; i++)
+    {
+        for (int j = 0; j < G->vertex_num; j++)
+        {
+            G->arc[i][j] = 0;
+        }
+    }
+
+    G->arc[0][1] = 1;
+    G->arc[0][5] = 1;
+
+    G->arc[1][2] = 1;
+    G->arc[1][6] = 1;
+    G->arc[1][8] = 1;
+
+    G->arc[2][3] = 1;
+    G->arc[2][8] = 1;
+
+    G->arc[3][4] = 1;
+    G->arc[3][6] = 1;
+    G->arc[3][7] = 1;
+    G->arc[3][8] = 1;
+
+    G->arc[4][5] = 1;
+    G->arc[4][7] = 1;
+
+    G->arc[5][6] = 1;
+    G->arc[6][7] = 1;
+
+    for (int i = 0; i < G->vertex_num; i++)
+    {
+        for (int j = 0; j < G->vertex_num; j++)
+        {
+            G->arc[j][i] = G->arc[i][j];
+        }
+    }
+}
+
+void dfs(Mat_Grph G, int i)
+{
+    visited[i] = 1;
+    printf("%c\n", G.vertex[i]);
+    for (int j = 0; j < G.vertex_num; j++)
+    {
+        if (G.arc[i][j] == 1 && visited[j] == 0)
+        {
+            dfs(G, j);
+        }
+    }
+}
+int main(void)
+{
+    Mat_Grph G;
+    create_graph(&G);
+    dfs(G, 0);
+
+    return 0;
+}
